31f14debcb7fda1c75ec7c416ff59c9a5def5909
Prem Suman <prem@cnapplications.com>
2018-05-07 17:11:27 +0530

    added notification icon

--- app/src/main/java/com/prembros/chatein/data/service/MyFirebaseMessagingService.java
+++ app/src/main/java/com/prembros/chatein/data/service/MyFirebaseMessagingService.java
@@ -3,17 +3,23 @@ package com.prembros.chatein.data.service;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.support.v4.app.NotificationCompat;
 
+import com.bumptech.glide.Glide;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import com.prembros.chatein.R;
 
+import java.lang.ref.WeakReference;
 import java.util.Objects;
 
 import static com.prembros.chatein.ui.social.ProfileActivity.USER_ID;
+import static com.prembros.chatein.util.Annotations.ChatType.IMAGE;
 import static com.prembros.chatein.util.Constants.DATA_URL;
 import static com.prembros.chatein.util.Constants.FROM_USER_ID;
+import static com.prembros.chatein.util.Constants.TYPE;
 
 public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
@@ -21,7 +27,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
         try {
             String body = Objects.requireNonNull(remoteMessage.getNotification()).getBody();
             NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, "BaseNotifications")
-                    .setSmallIcon(R.mipmap.ic_launcher)
+                    .setSmallIcon(R.drawable.ic_notification_icon)
                     .setContentTitle(Objects.requireNonNull(remoteMessage.getNotification()).getTitle())
                     .setContentText(body)
                     .setPriority(NotificationCompat.PRIORITY_DEFAULT);
@@ -30,9 +36,13 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 mBuilder.setStyle(new NotificationCompat.BigTextStyle().bigText(remoteMessage.getNotification().getBody()));
             }
 
+            if (Objects.equals(remoteMessage.getData().get(TYPE), IMAGE)) {
+                new LoadImageTask(this, remoteMessage.getData().get(DATA_URL), mBuilder);
+            }
+
             Intent resultIntent = new Intent(remoteMessage.getNotification().getClickAction());
             resultIntent.putExtra(USER_ID, remoteMessage.getData().get(FROM_USER_ID));
-            resultIntent.putExtra(DATA_URL, remoteMessage.getData().get(DATA_URL));
+            resultIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
             PendingIntent resultPendingIntent = PendingIntent.getActivity(this, 0,
                     resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
@@ -45,6 +55,39 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             }
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    private static class LoadImageTask extends AsyncTask<Void, Void, Bitmap> {
+
+        private WeakReference<MyFirebaseMessagingService> ref;
+        private String dataUrl;
+        private NotificationCompat.Builder mBuilder;
+
+        private LoadImageTask(MyFirebaseMessagingService ref, String dataUrl, NotificationCompat.Builder mBuilder) {
+            this.ref = new WeakReference<>(ref);
+            this.dataUrl = dataUrl;
+            this.mBuilder = mBuilder;
+        }
+
+        @Override protected Bitmap doInBackground(Void... voids) {
+            try {
+                return Glide.with(ref.get())
+                        .asBitmap()
+                        .load(dataUrl)
+                        .submit(100, 100)
+                        .get();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            if (bitmap != null) {
+                mBuilder.setLargeIcon(bitmap);
+            }
         }
     }
 }
